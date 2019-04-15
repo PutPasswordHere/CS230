ad994a109f4786cd669fffaa247c8f4dfa33e06f
PutPasswordHere <PutPasswordHere@users.noreply.github.com>
2019-04-15 14:07:18 -0400

    Updated managerMenu

---
+++ managerMenu.h
@@ -0,0 +1,28 @@
+//Dan Laukzemis Manager menu
+
+#ifndef MANAGERMENU_H
+#define MANAGERMENU_H
+class managerMenu{
+	public:
+		managerMenu();
+		void displayMenu();
+	private:
+		
+};
+
+managerMenu::managerMenu(){
+	int choice;
+	system("CLS");
+	cout <<"\tManager Menu\n"
+		 <<"\t=========\n"
+		 <<"\t1. View active repair orders\n"
+		 <<"\t2. Issue Invoice\n"
+		 <<"\t3. Sign off on repair order\n"
+		 <<"\t4. EXIT\n"
+		 <<"\tEnter your choice: ";
+	cin >> choice;
+	
+	return choice;
+}
+
+#endif
